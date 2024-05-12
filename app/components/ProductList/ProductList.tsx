f185aa6357bf0954811df718e13990693f32f208
zannain <zannainshafi@gmail.com>
2024-05-12 08:34:14 -0700

    more work on aligning grid

--- app/components/ProductList/ProductList.tsx
+++ app/components/ProductList/ProductList.tsx
@@ -7,19 +7,19 @@ type ProductListProps = {
 };
 export default function ProductList({ products }: ProductListProps) {
   return (
-    <div>
-      <div className="flex gap-2 m-2 justify-between">
-        <h1 className="text-lg font-bold mb-10 ml-4">Latest Arrivals</h1>
-        <Button size="lg" variant="secondary">
-          View All
-        </Button>
-      </div>
+    <div className="product-wrapper">
+        <div className="flex gap-2 m-2 justify-between self-stretch">
+          <h1 className="text-lg font-bold mb-10 ml-4">Latest Arrivals</h1>
+          <Button size="lg" variant="secondary">
+            View All
+          </Button>
+        </div>
 
-      <div className="container gap-y-8">
-        {products.map((product) => (
-          <Product key={product.product_id} product={product} />
-        ))}
-      </div>
+        <div className="container gap-y-8">
+          {products.map((product) => (
+            <Product key={product.product_id} product={product} />
+          ))}
+        </div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,33 +1,12 @@
-import type { Config } from 'tailwindcss'
-import colors from 'tailwindcss/colors'
+import type { Config } from "tailwindcss";
+import colors from "tailwindcss/colors";
 export default {
-  content: ['./app/**/*.{js,jsx,ts,tsx}'],
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-
     fontFamily: {
-      sans: ['Noto Sans']
+      sans: ["Noto Sans"],
     },
-    fontSize: {
-      'sm': ['0.875rem', {lineHeight: '1.25rem'}],
-      'lg': ['1.125rem', {lineHeight: '1.75rem'}],
-      '3xl': ['1.875rem', {lineHeight: '2.25rem'}]
-    },
-    extend: {
-      colors: {
-        white: colors.white,
-        black: colors.neutral[900],
-        neutral: {
-          600: colors.neutral[600],
-          500: colors.neutral[500],
-          400: colors.neutral[400]
-        },
-        yellow: colors.yellow[600],
-        orange: colors.orange[600],
-        teal: colors.teal[100],
-        silver: '#D2D6DB'
-      }
-    },
+    extend: {},
   },
   plugins: [],
 } satisfies Config;
-
